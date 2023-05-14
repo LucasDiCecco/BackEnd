5c1e05e0350eba7c197c735687b49b02026d8fa4
LucasDiCecco <lucasedicecco@gmail.com>
2023-05-14 11:51:17 -0300

    target

---
+++ src/test/java/com/portfolio/ldc/MgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ldc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class MgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
